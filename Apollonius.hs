0b0cb3d0a379107a60f6ab7214eda85a080d08e3
Salvador Fandiño <sfandino@yahoo.com>
2018-12-03 10:19:18 +0100

    wip: remove traces from Apollonius

--- Apollonius.hs
+++ Apollonius.hs
@@ -28,20 +28,20 @@ apollonius :: Circle -> Circle -> Circle -> [Circle]
 -- EQ₀₁: 2(x₀ - x₁)x + 2(y₀ - y₁)y = x₀² - x₁² + y₀² - y₁² - r₀² + r₁² - 2(r₀ - r₁)r = b₀₁ - 2r₀₁r
 -- EQ₀₂: 2(x₀ - x₂)x + 2(y₀ - y₂)y = x₀² - x₂² + y₀² - y₂² - r₀² + r₂² - 2(r₀ - r₂)r = b₀₂ - 2r₀₂r
 
--- det = 4((x₀ - x₁)(y₀ - y₂) - (y₀ - y₁)(x₀ - x₂))
+-- det₁ = 4((x₀ - x₁)(y₀ - y₂) - (y₀ - y₁)(x₀ - x₂))
 
 
 circleEq (Circle (xc :+ yc) rc) = fromList [xc*xc+yc*yc-rc*rc, -2*xc, -2*yc, -2*rc, 1, 1, -1]
 
-det eqA eqB ixA ixB = tr "det" $
+det eqA eqB ixA ixB =
   (eqA!ixA)*(eqB!ixB) - (eqA!ixB)*(eqB!ixA)
 
-bestDet eqA eqB permutations = tr "bestDet" $ foldl pickBest (0, (0,0,0)) $ map detAndPerm permutations
+bestDet eqA eqB permutations = foldl pickBest (0, (0,0,0)) $ map detAndPerm permutations
   where detAndPerm perm@(a, b, _) = (det eqA eqB a b, perm)
-        pickBest bestSoFar@(bestDet, bestPerm) current@(det, perm) = tr "bestSoFar" $
+        pickBest bestSoFar@(bestDet, bestPerm) current@(det, perm) =
           if (abs bestDet) >= (abs det) then bestSoFar else current
 
-comb s0 s1 v0 v1 = tr "comb" $ s0 * v0 + s1 * v1
+comb s0 s1 v0 v1 = s0 * v0 + s1 * v1
 
 sqrEq eq c c2 =
   let a0 = eq!0
@@ -62,48 +62,45 @@ sqrEq eq c c2 =
 
 solveEq2 a b c =
   if a == 0
-  then tr "sol10" $ [-c/b]
-  else let d = tr "d" $ b*b-4*a*c
+  then [-c/b]
+  else let d = b*b-4*a*c
        in if d < 0
-          then tr "sol0" $ []
+          then []
           else if d == 0
-               then tr "sol1" $ [-b/(2*a)]
-               else let sqrtD = tr "sqrtD" $ sqrt d
-                    in tr "sols" $ [(b + sqrtD)/(-2*a), (b - sqrtD)/(-2*a)]
-
-tr :: (Show b) => String -> b -> b
-tr s a = trace (s ++ ": " ++ (show a)) a
---tr _ a = a
+               then [-b/(2*a)]
+               else let sqrtD = sqrt d
+                    in [(b + sqrtD)/(-2*a), (b - sqrtD)/(-2*a)]
 
 apollonius circleA circleB circleC =
-  let eqA = tr "eqA" $ circleEq circleA
-      eqB = tr "eqB" $ circleEq circleB
-      eqC = tr "eqC" $ circleEq circleC
-      eqAB = tr "eqAB" $ VU.zipWith (-) eqA eqB
-      eqAC = tr "eqAC" $ VU.zipWith (-) eqA eqC
-      (det, (c0, c1, c2)) = tr "best" $ bestDet eqAB eqAC [(1,2,3), (2,3,1), (3,1,2)]
+  let eqA = circleEq circleA
+      eqB = circleEq circleB
+      eqC = circleEq circleC
+      eqAB = VU.zipWith (-) eqA eqB
+      eqAC = VU.zipWith (-) eqA eqC
+      (det, (c0, c1, c2)) = bestDet eqAB eqAC [(1,2,3), (2,3,1), (3,1,2)]
   in if det == 0
      then []
      else let idet = 1/det
-              eqAB1 = tr "eqAB1" $ VU.zipWith (comb (idet*(eqAC!c1)) (-idet*(eqAB!c1))) eqAB eqAC
-              eqAC1 = tr "eqAC1" $ VU.zipWith (comb (idet*(eqAB!c0)) (-idet*(eqAC!c0))) eqAC eqAB
-              eqAB2 = tr "eqAB2" $ sqrEq eqAB1 c0 c2
-              eqAC2 = tr "eqAC2" $ sqrEq eqAC1 c1 c2
-              c0_2 = tr "c0_2" $ c0 + 3
-              c1_2 = tr "c1_2" $ c1 + 3
-              c2_2 = tr "c2_2" $ c2 + 3
-              eqA1 = tr "eqA1" $ VU.zipWith (comb 1 (tr "a1" $ (-(eqA !c0_2)))) eqA eqAB2
-              eqA2 = tr "eqA2" $ VU.zipWith (comb 1 (tr "a2" $ (-(eqA1!c1_2)))) eqA1 eqAC2
-              eqA3 = tr "eqA3" $ VU.zipWith (comb 1 (tr "a3" $ (-(eqA2!c0)))) eqA2 eqAB1
-              eqA4 = tr "eqA4" $ VU.zipWith (comb 1 (tr "a4" $ (-(eqA3!c1)))) eqA3 eqAC1
-              a = tr "a" $ eqA4!c2_2
-              b = tr "b" $ eqA4!c2
-              c = tr "c" $ eqA4!0
-              v2s = tr "v2s" $ tr "solveEq2" $ solveEq2 a b c
-              v0s = tr "v0s" $ map (\v2 -> -(eqAB1!0 + v2*eqAB1!c2)) v2s
-              v1s = tr "v1s" $ map (\v2 -> -(eqAC1!0 + v2*eqAC1!c2)) v2s
-          in tr "result" $ filter (\(Circle _ r) -> r >= 0) $ case c0 of
+              eqAB1 = VU.zipWith (comb (idet*(eqAC!c1)) (-idet*(eqAB!c1))) eqAB eqAC
+              eqAC1 = VU.zipWith (comb (idet*(eqAB!c0)) (-idet*(eqAC!c0))) eqAC eqAB
+              eqAB2 = sqrEq eqAB1 c0 c2
+              eqAC2 = sqrEq eqAC1 c1 c2
+              c0_2 = c0 + 3
+              c1_2 = c1 + 3
+              c2_2 = c2 + 3
+              eqA1 = VU.zipWith (comb 1 (-(eqA !c0_2))) eqA eqAB2
+              eqA2 = VU.zipWith (comb 1 (-(eqA1!c1_2))) eqA1 eqAC2
+              eqA3 = VU.zipWith (comb 1 (-(eqA2!c0))) eqA2 eqAB1
+              eqA4 = VU.zipWith (comb 1 (-(eqA3!c1))) eqA3 eqAC1
+              a = eqA4!c2_2
+              b = eqA4!c2
+              c = eqA4!0
+              v2s = solveEq2 a b c
+              v0s = map (\v2 -> -(eqAB1!0 + v2*eqAB1!c2)) v2s
+              v1s = map (\v2 -> -(eqAC1!0 + v2*eqAC1!c2)) v2s
+          in filter positiveRadius $ case c0 of
                1 -> zipWith3 makeCircle v0s v1s v2s
                2 -> zipWith3 makeCircle v2s v0s v1s
                3 -> zipWith3 makeCircle v1s v2s v0s
   where makeCircle a b c = Circle (a :+ b) c
+        positiveRadius (Circle _ r) = r >= 0
